7a04bd0e174690264f19c3ff25d9d1b253d36741
Ed <edmundluong@gmail.com>
2019-03-06 19:56:21 -0800

    finished simple template vars

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,13 +3,58 @@
 console.log('App.js is running!');
 
 //JSX - JavaScript XML
+//must have a single root tag to display; usually wrap with a wrapper div
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Ed Luong'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 27'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: California'
+    )
+);
+
 var appRoot = document.getElementById('app');
 
 //first param is pass in template
 //second param is where to render
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -2,9 +2,28 @@ console.log('App.js is running!');
 
 
 //JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+//must have a single root tag to display; usually wrap with a wrapper div
+var template = (
+<div>
+    <h1>Indecision App</h1> 
+    <p>This is some info</p>
+    <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+    </ol>
+</div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Ed Luong</h1>
+        <p>Age: 27</p>
+        <p>Location: California</p>
+    </div>
+)
+
 var appRoot = document.getElementById('app');
 
 //first param is pass in template
 //second param is where to render
-ReactDOM.render(template,appRoot); 
+ReactDOM.render(templateTwo,appRoot); 
