68516d39718ed617307f62bbc54879d46d184ce0
Ed <edmundluong@gmail.com>
2019-03-12 23:55:39 -0700

    intro to events and attributes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,63 +1,112 @@
 'use strict';
 
-//Topics
-// arguments object - no longer bound with arrow functions
-// this keyword - no longer bound
+console.log('App.js is running!');
 
+//create app object
 
-// const add = function (a,b) {
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 
-//     console.log(arguments);
-//     return a + b;
+    //JSX - JavaScript XML
+    //must have a single root tag to display; usually wrap with a wrapper div
+};var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+// const user = {
+//     name: 'Ed',
+//     age: 27,
+//     location: 'California'
 // };
 
-//Converting to es6
-//below will crash, cannot access arguments; ReferenceError: arguments is not defined
-// const add = (a,b) =>{
-//     console.log(arguments);
-//     return a + b;
+
+// function getLocation(location) {
+//     if (location){
+//         return <p>Location: {location}</p>;
+//     } //can return undefined but it implicilty returns undefined
 // }
 
-var add = function add(a, b) {
-  return a + b;
+// const templateTwo = (
+//     <div>
+//         <h1>{user.name ? user.name : 'Anonymous'}</h1> 
+//         {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+//         {getLocation(user.location)}
+//     </div>
+// )
+var count = 0;
+
+var addOne = function addOne() {
+    console.log('addOne');
 };
 
-console.log(add(55, 1));
-
-//this keyword - no longer bound
-var user = {
-  name: 'Ed',
-  cities: ['Santa Barbara', 'Taipei'],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    //es6 syntax for methods
-    //const that = this; //common work around to access variables; in es6 this is not necessary anymore
-
-    //using the course way of printing out many items; transform on the items
-    return this.cities.map(function (city) {
-      return _this.name + ' has lived in ' + city;
-    }); //does not create a new array;
-  }
+var minusOne = function minusOne() {
+    console.log('minusOne');
 };
 
-console.log(user.printPlacesLived());
-
-//one example would to not change the printPlacesLived to use arrow function. This would go up to the parent scope, 
-//which cities does not exist.
-
-//challenge
-
-var multiplier = {
-  numbers: [1, 2, 3],
-  multiplyBy: 2,
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (num) {
-      return _this2.multiplyBy * num;
-    });
-  }
+var reset = function reset() {
+    console.log('reset');
 };
 
-console.log(multiplier.multiply());
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Count: ',
+        count
+    ),
+    React.createElement(
+        'button',
+        { onClick: addOne },
+        '+1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: minusOne },
+        '-1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: reset },
+        'reset'
+    )
+);
+
+var appRoot = document.getElementById('app');
+
+//first param is pass in template
+//second param is where to render
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -23,29 +23,52 @@ const template = (
 </div>
 );
 
-const user = {
-    name: 'Ed',
-    age: 27,
-    location: 'California'
+// const user = {
+//     name: 'Ed',
+//     age: 27,
+//     location: 'California'
+// };
+
+
+// function getLocation(location) {
+//     if (location){
+//         return <p>Location: {location}</p>;
+//     } //can return undefined but it implicilty returns undefined
+// }
+
+// const templateTwo = (
+//     <div>
+//         <h1>{user.name ? user.name : 'Anonymous'}</h1> 
+//         {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+//         {getLocation(user.location)}
+//     </div>
+// )
+let count = 0;
+
+const addOne = () =>{
+    console.log('addOne');
 };
 
-
-function getLocation(location) {
-    if (location){
-        return <p>Location: {location}</p>;
-    } //can return undefined but it implicilty returns undefined
+const minusOne = () =>{
+    console.log('minusOne');
 }
+
+const reset = () =>{
+    console.log('reset');
+}
+
 
 const templateTwo = (
     <div>
-        <h1>{user.name ? user.name : 'Anonymous'}</h1> 
-        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
+        <h1>Count: {count}</h1>
+        <button onClick={addOne}>+1</button>
+        <button onClick={minusOne}>-1</button>
+        <button onClick={reset}>reset</button>
     </div>
-)
+);
 
 const appRoot = document.getElementById('app');
 
 //first param is pass in template
 //second param is where to render
-ReactDOM.render(template,appRoot); 
+ReactDOM.render(templateTwo,appRoot); 
