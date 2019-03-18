5fde853f57ae3ebb72c2a695af7223af4fdd8628
Ed <edmundluong@gmail.com>
2019-03-17 21:23:50 -0700

    added button to select random option

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -28,6 +28,12 @@ var onRemoveAll = function onRemoveAll() {
     renderOptions();
 };
 
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    alert(option);
+};
+
 var appRoot = document.getElementById('app');
 
 var renderOptions = function renderOptions() {
@@ -52,9 +58,9 @@ var renderOptions = function renderOptions() {
             app.options.length > 0 ? 'Here are your options' : 'No options'
         ),
         React.createElement(
-            'p',
-            null,
-            app.options.length
+            'button',
+            { disabled: app.options.length === 0, onClick: onMakeDecision },
+            'What should I do?'
         ),
         React.createElement(
             'button',
