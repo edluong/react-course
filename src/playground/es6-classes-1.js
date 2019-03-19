6b53faffa0fadd965708c23522f2997d1f3dae79
Ed <edmundluong@gmail.com>
2019-03-19 00:17:23 -0700

    es6 classes part 2

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -11,8 +11,52 @@ class Person {
     }
 } 
 
-const me = new Person('Ed L',27);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name,age,major){
+        super(name,age);
+        this.major = major;
+    }
+    hasMajor(){
+        return !!this.major; //flipping the value to see if Student has a major; !'' = true, therefore double !!
+    }
+    //override the parent method
+    getDescription(){
+        let description = super.getDescription(); //get the parent's method
+        
+        if (this.hasMajor()){
+            description += ` Their major is ${this.major}.`;
+        }
 
-const other = new Person();
-console.log(other.getDescription());
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name,age,homeLocation){
+        super(name,age);
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting(){
+        
+        let greeting = super.getGreeting();
+        
+        if(this.homeLocation){
+            greeting += ` I'm visiting from ${this.homeLocation}.`;
+        }
+
+        return greeting;
+    }
+}
+
+// const me = new Student('Ed L',27,'Computer Science');
+// console.log(me.getDescription());
+
+// const other = new Student();
+// console.log(other.getDescription());
+
+const me = new Traveler('Ed L',27,'California');
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
