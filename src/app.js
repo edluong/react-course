9497263a647aaf0686b17311c8674cc4758cdeba
Ed <edmundluong@gmail.com>
2019-03-19 00:36:45 -0700

    react components

--- src/app.js
+++ src/app.js
@@ -1,69 +1,53 @@
-console.log('App.js is running!');
-
-
-//create app object
-
-const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: []
+class Header extends React.Component {
+    //must define render from a React Component
+    render(){
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
+    }
 }
 
-const onFormSubmit = (e) => {
-    e.preventDefault(); //stop full page refresh
-
-    const option = e.target.elements.option.value;    //point to the event that started on. (event object)
-
-    if (option){
-        app.options.push(option);
-        e.target.elements.option.value = ''; //reset the textbox to nothing again
-        renderOptions(); //re - render the screen
-    }
-};
-
-//remove all the elements from the options array in app
-const onRemoveAll = () => {
-    app.options.length = 0; //set the length of the array to 0; which will remove elements
-    renderOptions();
-};
-
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNum];
-    alert(option);
-};
-
-
-const appRoot = document.getElementById('app');
-
-
-const renderOptions = () => {
-    //JSX - JavaScript XML
-    //must have a single root tag to display; usually wrap with a wrapper div
-    const template = (
-        <div>
-            <h1>{app.title}</h1> 
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
-            <button onClick={onRemoveAll}>Remove All</button>
-            <ol>
-            {
-                app.options.map((option) => <li key={option}>{option}</li>)
-            }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-            <input type="text" name="option"/>
-            <button>Add Option</button> 
-            </form>
-        </div>
+class Action extends React.Component {
+    render(){
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
         );
+    }
+}
 
-    //render the template to the screen    
-    ReactDOM.render(template,appRoot);
-    
-};
+class Options extends React.Component {
+    render(){
+        return (
+            <div>
+                Options component here
+            </div>
+            );
+    }
+}
 
-// render the template the first time
-renderOptions();
+class AddOption extends React.Component {
+    render(){
+        return(
+            <div>
+                Add Option component here
+            </div>
+        );
+    }
+}
 
+const jsx = (
+    <div>
+        <Header /> 
+        <Action />
+        <Options />
+        <AddOption />
+    </div>
+);
+
+
+ReactDOM.render(jsx,document.getElementById('app'));
