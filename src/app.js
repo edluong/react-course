f759bff859c359d2fcb013be75589c215d248f8f
Ed <edmundluong@gmail.com>
2019-03-23 09:21:42 -0700

    wrote handle methods for Action and Option component

--- src/app.js
+++ src/app.js
@@ -1,24 +1,44 @@
-// const obj = {
-//     name: 'Ed',
-//     getName() {
-//         return this.name;
-//     }
-// }
-
-// //a common problem is losing this reference
-// const getName = obj.getName.bind({name:'Edmund'}); //bind() is a useful method in where we get the in scope variables, such as name
-//console.log(getName());
-
 class IndecisionApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handlePick = this.handlePick.bind(this);
+        this.state = {
+            options: ['Thing one','Thing two','Thing three']
+        };
+    }
+
+    //this method is to pass to the Options child so it can have access to IndecisionApps options array
+    handleDeleteOptions() {
+        this.setState(() => {
+            return {
+                options: []
+            };
+        });
+    }
+    
+    // method to pass down to Action Component child
+    // pick an option for the computer to do
+    handlePick() {
+       const randomNum = Math.floor(Math.random() * this.state.options.length);
+       const option = this.state.options[randomNum];
+       alert(option);
+    }
+
     render() {
         const title = 'Indecision';
         const subtitle = 'Put your life in the hands of a computer';
-        const options = ['Thing one','Thing two','Thing four'];
         return (
             <div>
                 <Header title={title} subtitle={subtitle} />
-                <Action />
-                <Options options={options} />
+                <Action 
+                    hasOptions={this.state.options.length > 0} 
+                    handlePick={this.handlePick}
+                />
+                <Options 
+                    options={this.state.options} 
+                    handleDeleteOptions={this.handleDeleteOptions}
+                />
                 <AddOption />  
             </div>
         );
@@ -39,14 +59,15 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
-
-    handlePick(){
-        alert('handlePick');
-    }
     render(){
         return (
             <div>
-                <button onClick={this.handlePick}>What should I do?</button>
+                <button 
+                onClick={this.props.handlePick}
+                disabled={!this.props.hasOptions}
+                >
+                What should I do?
+                </button>
             </div>
         );
     }
@@ -65,19 +86,11 @@ class Option extends React.Component {
 
 class Options extends React.Component {
     
-    //this is done to use bind() handleremove all. need to override react.component
-    constructor(props) {
-        super(props);
-        this.handleRemoveAll = this.handleRemoveAll.bind(this);
-    }
-
-    handleRemoveAll(){
-        console.log(this.props.options);
-    }
+  
     render(){
         return (
             <div>
-                <button onClick={this.handleRemoveAll.bind(this)}>Remove All</button>
+                <button onClick={this.props.handleDeleteOptions}>Remove All</button>
                 {
                   this.props.options.map((option) => <Option key={option} optionText={option} />)
                 }
@@ -112,9 +125,5 @@ class AddOption extends React.Component {
         );
     }
 }
-
-
-
-
 
 ReactDOM.render(<IndecisionApp />,document.getElementById('app'));
