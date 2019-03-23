6f0b2d7a9c09708e790cc0cb52ee39e1615da26d
Ed <edmundluong@gmail.com>
2019-03-22 21:43:06 -0700

    react component state

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,55 +1,111 @@
-// const user = {
-//     name: 'Ed',
-//     age: 27,
-//     location: 'California'
+ class Counter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {   //setting up the default state
+            count: 0
+        };
+    }
+
+    handleAddOne(){
+        //used because we do not manually update such as this.state.count = this.state.count + 1
+        //should use the method setState
+        //prevState is a function parameter. Can be named anything; functionally everyone names it prevState
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 1 //this return statement is only updating the values given; returning an object
+            }; 
+        });   
+    };
+
+    handleMinusOne(){
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            };
+        })
+    };
+
+    handleReset() {
+        this.setState(() => {
+            return {
+                count: 0
+            };
+        })
+    };
+    
+    
+    render() {
+        return (
+            <div>
+                <h1>Count: {this.state.count} </h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>Reset</button>
+            </div>
+        );
+    }
+ };
+
+ ReactDOM.render(<Counter />,document.getElementById('app'));
+
+
+
+// // const user = {
+// //     name: 'Ed',
+// //     age: 27,
+// //     location: 'California'
+// // };
+
+
+// // function getLocation(location) {
+// //     if (location){
+// //         return <p>Location: {location}</p>;
+// //     } //can return undefined but it implicilty returns undefined
+// // }
+
+// // const templateTwo = (
+// //     <div>
+// //         <h1>{user.name ? user.name : 'Anonymous'}</h1> 
+// //         {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+// //         {getLocation(user.location)}
+// //     </div>
+// // )
+// let count = 0;
+
+// const addOne = () =>{
+//     count++;
+//     renderCounterApp();
 // };
 
-
-// function getLocation(location) {
-//     if (location){
-//         return <p>Location: {location}</p>;
-//     } //can return undefined but it implicilty returns undefined
+// const minusOne = () => {
+//     count--;
+//     renderCounterApp();
 // }
 
-// const templateTwo = (
-//     <div>
-//         <h1>{user.name ? user.name : 'Anonymous'}</h1> 
-//         {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-//         {getLocation(user.location)}
-//     </div>
-// )
-let count = 0;
-
-const addOne = () =>{
-    count++;
-    renderCounterApp();
-};
-
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-}
-
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-}
+// const reset = () => {
+//     count = 0;
+//     renderCounterApp();
+// }
 
 
 
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    );
+// const renderCounterApp = () => {
+//     const templateTwo = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={minusOne}>-1</button>
+//             <button onClick={reset}>reset</button>
+//         </div>
+//     );
 
-//first param is pass in template
-//second param is where to render
-ReactDOM.render(templateTwo,appRoot); 
-};
+// //first param is pass in template
+// //second param is where to render
+// ReactDOM.render(templateTwo,appRoot); 
+// };
 
-renderCounterApp();
+// renderCounterApp();
